729f1e32ab3887feca4e044f8d18474ba8e68495
Lucas Combs <l-combs@hotmail.com>
2019-03-13 21:36:42 -0400

    Initial Commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = 'C:\\Users\\lucas\\Dropbox\\CSC440\\ProjectPreparations\\static\\content'
+CONTENT_DIR = 'C:\\Users\\lucas\\Dropbox\\CSC440\\ProjectPreparations\\Wiki440\\content'
+USER_DIR = 'C:\\Users\\lucas\\Dropbox\\CSC440\\ProjectPreparations\\Wiki440\\user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
